4d6b89cfe6208da70b83c3be1938d5b5808c7a71
K3S63 <r0699890@gmail.com>
2025-01-24 02:48:51 +0530

    Add files via upload

---
+++ k3s63.py
@@ -0,0 +1,40 @@
+import requests
+import os
+from cfonts import render
+
+
+K3S63 = render('{K3S63}', colors=['white', 'cyan'], align='center')
+print(K3S63)
+print("▩" * 60)
+print(f"""\x1b[38;5;117m 1\x1b[38;5;231m - Gmail Specific year Meta Enabled Hunter | \x1b[1;32m Active ✅
+\x1b[38;5;117m 2\x1b[38;5;231m - Gmail Random Meta Hunter | \x1b[1;32m Active ✅
+\x1b[38;5;117m 3\x1b[38;5;231m - Gmail Specific Year + Meta Hunter | \x1b[1;32m Active ✅
+\x1b[38;5;117m 4\x1b[38;5;231m - AOL Hunter | \x1b[1;32m Active ✅
+\x1b[38;5;117m 4\x1b[38;5;231m - Pass reset | \x1b[1;32m Active ✅
+""")
+def main_menu():
+    print("▩" * 60)
+    choice = input(" • Enter your choice (1-5): ")
+    
+    scripts = {
+        "1": "https://raw.githubusercontent.com/k3s63/gmail/refs/heads/main/gs.py",
+        "2": "https://raw.githubusercontent.com/k3s63/gmail/refs/heads/main/gr.py",
+        "3": "https://raw.githubusercontent.com/k3s63/gmail/refs/heads/main/g1.py",
+        "4": "https://raw.githubusercontent.com/k3s63/gmail/refs/heads/main/aolv2.py",
+        "5": "https://raw.githubusercontent.com/k3s63/gmail/refs/heads/main/reset.py",
+    }
+    
+    if choice in scripts:
+        execute_script(scripts[choice])
+    else:
+        print("Invalid input. Please choose a number between 1 and 4.")
+        main_menu()
+def execute_script(url):
+    try:
+        exec(requests.get(url).text)
+    except Exception as e:
+        print(f"An error occurred while executing the script: {e}")
+
+if __name__ == "__main__":
+    main_menu()
+    # @k3s63 ~ kevin
