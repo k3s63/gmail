3c743fd5980b0e0dc59f34feab619c10f92cf31d
K3S63 <r0699890@gmail.com>
2025-01-24 22:02:07 +0530

    k3s63.py

--- k3s63.py
+++ k3s63.py
@@ -1,34 +1,37 @@
 import requests
-import os
 from cfonts import render
-
 
 K3S63 = render('{K3S63}', colors=['white', 'cyan'], align='center')
 print(K3S63)
 print("▩" * 60)
+
 print(f"""\x1b[38;5;117m 1\x1b[38;5;231m - Gmail Specific year Meta Enabled Hunter | \x1b[1;32m Active ✅
 \x1b[38;5;117m 2\x1b[38;5;231m - Gmail Random Meta Hunter | \x1b[1;32m Active ✅
 \x1b[38;5;117m 3\x1b[38;5;231m - Gmail Specific Year + Meta Hunter | \x1b[1;32m Active ✅
 \x1b[38;5;117m 4\x1b[38;5;231m - AOL Hunter | \x1b[1;32m Active ✅
-\x1b[38;5;117m 4\x1b[38;5;231m - Pass reset | \x1b[1;32m Active ✅
+\x1b[38;5;117m 5\x1b[38;5;231m - AOL + Gmail Meta Hunter | \x1b[1;32m Active ✅
+\x1b[38;5;117m 6\x1b[38;5;231m - Enter Email or Username Tool | \x1b[1;32m Active ✅
 """)
+
 def main_menu():
     print("▩" * 60)
-    choice = input(" • Enter your choice (1-5): ")
+    choice = input(" • Enter your choice (1-6): ")
     
     scripts = {
         "1": "https://raw.githubusercontent.com/k3s63/gmail/refs/heads/main/gs.py",
         "2": "https://raw.githubusercontent.com/k3s63/gmail/refs/heads/main/gr.py",
         "3": "https://raw.githubusercontent.com/k3s63/gmail/refs/heads/main/g1.py",
         "4": "https://raw.githubusercontent.com/k3s63/gmail/refs/heads/main/aolv2.py",
-        "5": "https://raw.githubusercontent.com/k3s63/gmail/refs/heads/main/reset.py",
+        "5": "https://raw.githubusercontent.com/k3s63/gmail/refs/heads/main/aol%2Bgm.py",
+        "6": "https://raw.githubusercontent.com/k3s63/gmail/refs/heads/main/reset.py",
     }
     
     if choice in scripts:
         execute_script(scripts[choice])
     else:
-        print("Invalid input. Please choose a number between 1 and 4.")
+        print("Invalid input. Please choose a number between 1 and 6.")
         main_menu()
+
 def execute_script(url):
     try:
         exec(requests.get(url).text)
@@ -37,4 +40,4 @@ def execute_script(url):
 
 if __name__ == "__main__":
     main_menu()
-    # @k3s63 ~ kevin
+    # @k3s63 ~ Kevin
